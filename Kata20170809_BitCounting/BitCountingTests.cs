d8ccd43efc45b9c31b6f605265ef929961cc0575
cash <cash@cashwu.com>
2017-08-09 14:12:31 +0800

    input 0 test case

--- Kata20170809_BitCounting/BitCountingTests.cs
+++ Kata20170809_BitCounting/BitCountingTests.cs
@@ -7,8 +7,24 @@ namespace Kata20170809_BitCounting
     public class BitCountingTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void input_0_should_reutrn_0()
         {
+            CountBetsShouldBe(0, 0);
+        }
+
+        private static void CountBetsShouldBe(int expected, int number)
+        {
+            var kata = new Kata();
+            var actual = kata.CountBits(number);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+    public class Kata
+    {
+        public int CountBits(int n)
+        {
+            return 0;
         }
     }
 }
